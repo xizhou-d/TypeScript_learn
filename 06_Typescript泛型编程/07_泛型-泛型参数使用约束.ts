f452034aa9cb827c0a2bc9f7dd8a7b8956b59493
XIZHOU <51404971+yangkun1994@users.noreply.github.com>
2024-03-14 22:17:29 +0800

    增加 keyof 的解释

--- 06_Typescript泛型编程/07_泛型-泛型参数使用约束.ts
+++ 06_Typescript泛型编程/07_泛型-泛型参数使用约束.ts
@@ -4,6 +4,16 @@ interface IKun {
     name: string
     age: number
 }
+/** 来自 Gemini 的解释
+ * 在 TypeScript 中，对象的属性名称必须是 keyof any 类型的。keyof any 类型表示所有对象属性的名称类型。
+
+    Record<K, T> 类型的 K 类型必须是 keyof any 类型。因此，Record<any, any> 类型的 K 类型也必须是 keyof any 类型。
+
+    keyof any 类型等同于 string | number | symbol 类型。也就是说，对象的属性名称只能是字符串、数字或 symbol 类型。
+
+    因此，Record<any, any> 类型表示一个对象，其中所有属性的名称和值的类型都是任意类型，但属性名称必须是字符串、数字或 symbol 类型。
+ */
+
 // IKumKeys 的类型是一个联合类型，就是联合了 IKun 中所有 key 的属性名  =>  "name"|"age"
 type IKunKeys = keyof IKun
 
